ecf5e5015b657acb5ec803301fb66834772a0ebd
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2024-03-08 02:54:20 +0900

    Following Camera

---
+++ 2024-Local-Competition/Assets/Scripts/Camera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float _followSpeed;
+    public GameObject _cameraTarget;
+    public GameObject _followCamera;
+
+    void Update()
+    {
+        if (_cameraTarget == null || _followCamera == null)
+            return;
+
+        transform.LookAt(_cameraTarget.transform);
+        transform.position = Vector3.Lerp(transform.position, _followCamera.transform.position, Time.deltaTime * _followSpeed);
+    }
+}
